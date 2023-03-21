eb289c7c2f3a5a203a0321d053a8fe59303b37e2
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-03-21 15:42:31 -0300

    GraphQL

--- src/resolver/Appointments-resolver.ts
+++ src/resolver/Appointments-resolver.ts
@@ -1,10 +1,35 @@
 import { Resolver } from "type-graphql";
-import { Query } from "type-graphql/dist/decorators";
+import { Arg, FieldResolver, Mutation, Query, Root } from "type-graphql/dist/decorators";
+import { CreateAppoimentInput } from "../dtos/inputs/create-appoiment-input";
+import { Appointment } from "../dtos/models/appointment-model";
+import { Customer } from "../dtos/models/custommer-model";
 
-@Resolver()
-export class AppointmentsResolver{
-    @Query(() => String)
-    async first() {
-    return 'primeira'
-}
+@Resolver(() => Appointment)
+export class AppointmentsResolver {
+  @Query(() => [Appointment])
+  async Appointment() {
+      return [
+        {
+          startAt: new Date(),
+          endsAt: new Date()
+        },
+      ];
+  }
+
+  @Mutation(() => Appointment)
+  async creatAppointments(@Arg('data') data: CreateAppoimentInput) {
+    const appointment = {
+      startAt: data.startAt,
+      endsAt: data.endsAt,
+    };
+    return appointment;
+    }
+
+    @FieldResolver(() => Customer)
+    async customer(@Root() appointment: Appointment) {
+        return {
+            name: 'Ben Hur'
+        }
+        
+    }
 }
